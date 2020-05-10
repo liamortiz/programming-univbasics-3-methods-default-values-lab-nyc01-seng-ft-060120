6785220aa0d9638d16e4878d19c63eb1667a5ccb
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-10 23:52:49 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,5 +2,5 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice(veg1, veg2, protein="tofu")
-  
-  
+  "What a nutritious meal!"
+  "A plate of #{protein} with #{veg1} and #{veg2}"
